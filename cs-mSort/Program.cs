b10b7fb9f23bf4039155eaaa71719b2bcec41b28
magnum-z <zr260578jea@gmail.com>
2017-07-19 00:13:53 +0300

    Moved quicksort to individual class QuickSort

--- cs-mSort/Program.cs
+++ cs-mSort/Program.cs
@@ -8,69 +8,18 @@ namespace cs_mSort
 {
     class Program
     {
-        Random _pivotRng = new Random();
-
-        void Swap(int[] items, int left, int right)
-        {
-            if (left != right)
-            {
-                int temp = items[left];
-                items[left] = items[right];
-                items[right] = temp;
-            }
-        }
-
-        public void Sort(int[] items)
-        {
-            quicksort(items, 0, items.Length - 1);
-        }
-
-        private void quicksort(int[] items, int left, int right)
-        {
-            if (left < right)
-            {
-                int pivotIndex = _pivotRng.Next(left, right);
-                int newPivot = partition(items, left, right, pivotIndex);
-
-                quicksort(items, left, newPivot - 1);
-                quicksort(items, newPivot + 1, right);
-            }
-        }
-
-        private int partition(int[] items, int left, int right, int pivotIndex)
-        {
-            int pivotValue = items[pivotIndex];
-
-            Swap(items, pivotIndex, right);
-
-            int storeIndex = left;
-
-            for (int i = left; i < right; i++)
-            {
-                if (items[i].CompareTo(pivotValue) < 0)
-                {
-                    Swap(items, i, storeIndex);
-                    storeIndex += 1;
-                }
-            }
-
-            Swap(items, storeIndex, right);
-            return storeIndex;
-        }        
-
         static void Main(string[] args)
         {
-            Program p = new Program();
+            QuickSort sort = new QuickSort();
 
             int[] ar = { 6, 4, 7, 1, 2, 8, 3, 5, 9, 0 };
 
-            p.Sort(ar);
+            sort.Sort(ar);
 
             for(int i = 0; i < 10; i++)
             {
-                Console.WriteLine(ar[i]);
-            }
-            
+                Console.WriteLine(ar[i]);                
+            }            
         }
     }
 }
---
+++ cs-mSort/QuickSort.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_mSort
+{
+    class QuickSort
+    {
+        Random _pivotRng = new Random();
+
+        void Swap(int[] items, int left, int right)
+        {
+            if (left != right)
+            {
+                int temp = items[left];
+                items[left] = items[right];
+                items[right] = temp;
+            }
+        }
+
+        public void Sort(int[] items)
+        {
+            quicksort(items, 0, items.Length - 1);
+        }
+
+        private void quicksort(int[] items, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotIndex = _pivotRng.Next(left, right);
+                int newPivot = partition(items, left, right, pivotIndex);
+
+                quicksort(items, left, newPivot - 1);
+                quicksort(items, newPivot + 1, right);
+            }
+        }
+
+        private int partition(int[] items, int left, int right, int pivotIndex)
+        {
+            int pivotValue = items[pivotIndex];
+
+            Swap(items, pivotIndex, right);
+
+            int storeIndex = left;
+
+            for (int i = left; i < right; i++)
+            {
+                if (items[i].CompareTo(pivotValue) < 0)
+                {
+                    Swap(items, i, storeIndex);
+                    storeIndex += 1;
+                }
+            }
+
+            Swap(items, storeIndex, right);
+            return storeIndex;
+        }
+
+    }
+}
